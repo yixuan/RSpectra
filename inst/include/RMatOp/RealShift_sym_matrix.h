83ebabc0ca85766965e395f82db3b6d5ddfda0db
Yixuan Qiu <yixuanq@gmail.com>
2019-04-05 21:28:40 -0400

    use LAPACK for LDLT factorization

--- inst/include/RMatOp/RealShift_sym_matrix.h
+++ inst/include/RMatOp/RealShift_sym_matrix.h
@@ -2,52 +2,79 @@
 #define REALSHIFT_SYM_MATRIX_H
 
 #include <RcppEigen.h>
+#include <R_ext/Lapack.h>
+#include "RealShift.h"
 
 class RealShift_sym_matrix: public RealShift
 {
 private:
     typedef Eigen::MatrixXd Matrix;
     typedef Eigen::VectorXd Vector;
+    typedef Eigen::VectorXi IntVector;
     typedef Eigen::Map<Eigen::MatrixXd> MapMat;
-    typedef Eigen::Map<const Eigen::VectorXd> MapConstVec;
-    typedef Eigen::Map<Eigen::VectorXd> MapVec;
-    typedef Eigen::LDLT<Eigen::MatrixXd> LDLTSolver;
+    typedef Eigen::Map<const Matrix> MapConstMat;
+    typedef Eigen::Map<const Vector> MapConstVec;
+    typedef Eigen::Map<Vector> MapVec;
 
-    MapMat     mat;
     const int  n;
     const char uplo;
-    LDLTSolver solver;
+    Matrix     fac;   // store the LDLT factorization result
+    IntVector  perm;  // store the LDLT factorization result
 
 public:
     RealShift_sym_matrix(SEXP mat_, const int nrow_, const char uplo_ = 'L') :
-        mat(REAL(mat_), nrow_, nrow_),
-        n(nrow_),
-        uplo(uplo_)
-    {}
+        n(nrow_), uplo(uplo_), fac(nrow_, nrow_), perm(nrow_)
+    {
+        fac.noalias() = MapConstMat(REAL(mat_), nrow_, nrow_);
+    }
 
     int rows() const { return n; }
     int cols() const { return n; }
 
+    // Use LAPACK to do the LDLT factorization
     void set_shift(double sigma)
     {
-        // Backup the diagonal elements
-        Vector diag = mat.diagonal();
-        mat.diagonal().array() -= sigma;
+        fac.diagonal().array() -= sigma;
 
-        if(uplo == 'L')
-            solver.compute(mat.selfadjointView<Eigen::Lower>());
-        else
-            solver.compute(mat.selfadjointView<Eigen::Upper>());
+        int lwork = -1, info;
+        double blocksize;
 
-        mat.diagonal() = diag;
+        // Inquire the optimal block size
+        F77_CALL(dsytrf)(&uplo, &n,
+                 fac.data(), &n,
+                 perm.data(),
+                 &blocksize, &lwork, &info);
+
+        if(info != 0)
+            Rcpp::stop("RealShift_sym_matrix: factorization failed with the given shift");
+
+        lwork = int(blocksize);
+        std::vector<double> work;
+        work.resize(lwork);
+
+        // LDLT factorization
+        F77_CALL(dsytrf)(&uplo, &n,
+                 fac.data(), &n,
+                 perm.data(),
+                 &work[0], &lwork, &info);
+
+        if(info != 0)
+            Rcpp::stop("RealShift_sym_matrix: factorization failed with the given shift");
     }
 
     // y_out = inv(A - sigma * I) * x_in
     void perform_op(const double* x_in, double* y_out)
     {
-        MapConstVec x(x_in, n);
-        MapVec y(y_out, n);
-        y.noalias() = solver.solve(x);
+        std::copy(x_in, x_in + n, y_out);
+
+        const int nrhs = 1;
+        int info;
+        F77_CALL(dsytrs)(&uplo, &n, &nrhs,
+                 fac.data(), &n,
+                 perm.data(), y_out, &n, &info);
+
+        if(info != 0)
+            Rcpp::stop("RealShift_sym_matrix: input vector has illegal values");
     }
 };
 
