8d0147c86be3ea5753d7ff93ce3c9af3014dcbbb
Yixuan Qiu <yixuanq@gmail.com>
2019-05-30 12:25:06 -0400

    also fix the real shift operator

--- inst/include/RMatOp/RealShift_sym_sparseMatrix.h
+++ inst/include/RMatOp/RealShift_sym_sparseMatrix.h
@@ -4,6 +4,41 @@
 #include <RcppEigen.h>
 #include "RealShift.h"
 
+// Mapping a dgCMatrix/dsCMatrix/dgRMatrix/dsRMatrix R object to Eigen
+// Default is ColMajor
+template <int Storage>
+inline Eigen::Map< Eigen::SparseMatrix<double, Storage> > map_sparse(SEXP mat)
+{
+    Rcpp::S4 obj(mat);
+    if(!(obj.is("dgCMatrix") || obj.is("dsCMatrix")))
+        throw std::invalid_argument("Need S4 class dgCMatrix or dsCMatrix for a mapped sparse matrix");
+
+    Rcpp::IntegerVector dim(obj.slot("Dim")), i(obj.slot("i")), p(obj.slot("p"));
+    Rcpp::NumericVector x(obj.slot("x"));
+
+    return Eigen::Map< Eigen::SparseMatrix<double, Storage> >(
+            dim[0], dim[1], p[dim[1]], p.begin(), i.begin(), x.begin()
+    );
+}
+
+// Specialization for RowMajor
+template <>
+inline Eigen::Map< Eigen::SparseMatrix<double, Eigen::RowMajor> > map_sparse<Eigen::RowMajor>(SEXP mat)
+{
+    Rcpp::S4 obj(mat);
+    if(!(obj.is("dgRMatrix") || obj.is("dsRMatrix")))
+        throw std::invalid_argument("Need S4 class dgRMatrix or dsRMatrix for a mapped sparse matrix");
+
+    Rcpp::IntegerVector dim(obj.slot("Dim")), j(obj.slot("j")), p(obj.slot("p"));
+    Rcpp::NumericVector x(obj.slot("x"));
+
+    return Eigen::Map< Eigen::SparseMatrix<double, Eigen::RowMajor> >(
+            dim[0], dim[1], p[dim[1]], p.begin(), j.begin(), x.begin()
+    );
+}
+
+
+
 template <int Storage>
 class RealShift_sym_sparseMatrix: public RealShift
 {
@@ -22,7 +57,7 @@ private:
 
 public:
     RealShift_sym_sparseMatrix(SEXP mat_, const int nrow_, const char uplo_ = 'L') :
-        mat(Rcpp::as<MapSpMat>(mat_)),
+        mat(map_sparse<Storage>(mat_)),
         n(nrow_),
         uplo(uplo_)
     {}
